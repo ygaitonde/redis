b57c9f27edbf763a30413fd4d9adee7074535ad9
yash.gaitonde@moderntreasury.com <gaitonde@umich.edu>
2022-12-07 21:48:06 -0500

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("128.0.0.1:6079").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {

fa4217a6d15ae2232f945225129cee65af344d6e
yash.gaitonde@moderntreasury.com <gaitonde@umich.edu>
2022-12-07 22:01:53 -0500

    respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,8 +11,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let pong = "+PONG\r\n";
+                _stream.write_all(pong.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
